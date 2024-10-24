a7082376fa2b2ef3803fef578bffc8acdcfe3625
Matt <mattleeder@posteo.co.uk>
2024-10-24 15:29:12 +0100

    Changed logic for finding active dropdown to use getElementsByClassName and move javascript out of html file

--- rail_national/static/javascript/base.js
+++ rail_national/static/javascript/base.js
@@ -22,22 +22,57 @@ for (var i = 0; i < listItems.length; i++) {
   console.log("Set active");
   var grandparentNode = listItems[i].parentNode.parentNode;
 
-  // Check if node is in a dropdown container
-  if (grandparentNode.classList.contains("dropdown-container")) {
-    console.log("Dropdown active");
-
-    // Toggle the active state without the transition period
-    var originalTransitionDuration = grandparentNode.style.transitionDuration;
-    grandparentNode.style.transitionDuration = "0s";
-    grandparentNode.classList.toggle("dropdown-container-visible");
-    setTimeout(() => {
-      grandparentNode.style.transitionDuration = originalTransitionDuration;
-    }, 50);
-
-    // Draw the correct icon and set to the correct state
-    var svgIcon = grandparentNode.previousElementSibling.getElementsByTagName("svg")[0];
-    svgIcon.innerHTML = `<path d="M200-440v-80h560v80H200Z"/>`;
-    grandparentNode.previousElementSibling.dataset.iconState = 1;
-  }
   break;
+}
+
+const svgPlusIcon = `<path d="M440-440H200v-80h240v-240h80v240h240v80H520v240h-80v-240Z"/>`;
+const svgMinusIcon = `<path d="M200-440v-80h560v80H200Z"/>`;
+
+var dropdownButtons = document.getElementsByClassName("dropdown-button");
+for (var i = 0; i < dropdownButtons.length; i++) {
+
+    // Check if dropdown has active link
+    var dropDownContainer = dropdownButtons[i].nextElementSibling;
+    if (dropDownContainer.getElementsByClassName("active").length > 0) {
+
+        // Toggle the active state without the transition period
+        var originalTransitionDuration = dropDownContainer.style.transitionDuration;
+        dropDownContainer.style.transitionDuration = "0s";
+        dropDownContainer.classList.toggle("dropdown-container-visible");
+        setTimeout(() => {
+            dropDownContainer.style.transitionDuration = originalTransitionDuration;
+        }, 50);
+
+        // Draw the correct icon and set to the correct state
+        var svgIcon = dropdownButtons[i].getElementsByTagName("svg")[0];
+        svgIcon.innerHTML = `<path d="M200-440v-80h560v80H200Z"/>`;
+        dropdownButtons[i].dataset.iconState = 1;
+
+    // Otherwise just set iconState to 0
+    } else {
+        dropdownButtons[i].dataset.iconState = 0;
+    }
+
+    // Add click listener to dropdown buttons
+    dropdownButtons[i].addEventListener("click", function() {
+
+        // Toggle visibility
+        this.classList.toggle("active-dropdown-button");
+        var dropdownContent = this.nextElementSibling;
+
+        console.log("Checking height");
+        console.log(dropdownContent);
+        dropdownContent.classList.toggle("dropdown-container-visible");
+
+        // Toggle icon
+        var svgIcon = this.getElementsByTagName("svg")[0];
+        console.log(svgIcon);
+        if (this.dataset.iconState == 0) {
+            svgIcon.innerHTML = svgMinusIcon;
+            this.dataset.iconState = 1;
+        } else {
+            svgIcon.innerHTML = svgPlusIcon;
+            this.dataset.iconState = 0;
+        }
+    });
 }
