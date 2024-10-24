03fb5bde0edff8a877a1f392fa8cc676df182933
Matt <mattleeder@posteo.co.uk>
2024-10-24 02:25:21 +0100

    Changed active navbar link code to check if part of a dropdown, if so toggle the dropdown to active

--- rail_national/static/javascript/base.js
+++ rail_national/static/javascript/base.js
@@ -15,8 +15,29 @@ console.log("Looping");
 console.log(listItems);
 for (var i = 0; i < listItems.length; i++) {
   console.log(listItems[i].childNodes[0].href);
-  if (listItems[i].childNodes[0].href == currentURL) {
-    listItems[i].className += " active";
-    console.log("Set active");
+  if (listItems[i].childNodes[0].href != currentURL) {
+    continue;
   }
+  listItems[i].className += " active";
+  console.log("Set active");
+  var grandparentNode = listItems[i].parentNode.parentNode;
+
+  // Check if node is in a dropdown container
+  if (grandparentNode.classList.contains("dropdown-container")) {
+    console.log("Dropdown active");
+
+    // Toggle the active state without the transition period
+    var originalTransitionDuration = grandparentNode.style.transitionDuration;
+    grandparentNode.style.transitionDuration = "0s";
+    grandparentNode.classList.toggle("dropdown-container-visible");
+    setTimeout(() => {
+      grandparentNode.style.transitionDuration = originalTransitionDuration;
+    }, 50);
+
+    // Draw the correct icon and set to the correct state
+    var svgIcon = grandparentNode.previousElementSibling.getElementsByTagName("svg")[0];
+    svgIcon.innerHTML = `<path d="M200-440v-80h560v80H200Z"/>`;
+    grandparentNode.previousElementSibling.dataset.iconState = 1;
+  }
+  break;
 }
